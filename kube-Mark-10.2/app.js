d714f2120243718712c68c99397d76b40cbdb839
saikamal33 <kamaleshsai33@gmail.com>
2025-02-11 18:34:38 +0530

    update image

--- kube-Mark-10.2/app.js
+++ kube-Mark-10.2/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the app!');
+});
+
 app.get('/status', (req, res) => {
   res.json({ message: 'App is running!' });
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
 
