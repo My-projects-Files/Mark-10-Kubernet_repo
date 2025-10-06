bf3ebcfb3df28d61db6fe638694cd4034f55d3c6
saikamal33 <kamaleshsai33@gmail.com>
2025-10-06 15:16:02 +0530

    vaulting config for k8s

--- kube-Mark-10.8/vault/vault-init.sh
+++ kube-Mark-10.8/vault/vault-init.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+#set this before running
+export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_TOKEN='root'
+
+
 # Enable postgrSQL secrets engine
 
 vault secrets enable database
@@ -12,5 +19,23 @@ vault write database/config/my-postgres-db \
 # Create a dynamic role
 
 value write database/roles/readonly-role \
-	db_name=my-postgres-db
-		
+	db_name=my-postgres-db \
+	creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNIT '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+	default_ttl="1h"\
+	max_ttl="24h"
+
+# Enable kubernetes auth
+vault auth enable kubernetes
+
+#configure kubernetes auth
+
+vault write auth/kubernetes/config \
+	token_reviewer_jwt="<service_account_token>" \
+	kubernetes_host="https://kubernetes.default.svc" \
+	kubernetes_ca_cert=@/path/to/ca.crt
+
+#Create role for app
+vault write auth/kubernetes/role/db-app \
+	bound_service_account_names="vault-auth" \
+	bound_servvice_access_namespaces="default" \
+	ttl="1h"
